33e38baa9e267d4ba21220a57db652316fa5bab7
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-10-17 17:04:09 -0700

    Wrote methods for Video game service class

--- src/main/java/edu/greenriver/sdev/saasproject/services/VideoGameService.java
+++ src/main/java/edu/greenriver/sdev/saasproject/services/VideoGameService.java
@@ -1,4 +1,65 @@
 package edu.greenriver.sdev.saasproject.services;
 
+import edu.greenriver.sdev.saasproject.models.VideoGame;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
 public class VideoGameService {
+    private List<VideoGame> videoGames = new ArrayList<>(List.of(
+        new VideoGame("Wii", "Wii Fit", false),
+        new VideoGame("Oculus", "In Death: Unchained", false),
+        new VideoGame("Switch", "Mario Party", true)
+    ));
+
+
+    public List<VideoGame> getAllVideoGames(){
+        return videoGames;
+    }
+
+    public VideoGame getVideoGameById(int id){
+        Optional<VideoGame> found = videoGames.stream()
+                .filter(videoGame -> videoGame.getId()==id)
+                .findFirst();
+        return found.orElse(null);
+    }
+
+    public VideoGame random(){
+        Random rand = new Random();
+        return videoGames.get(rand.nextInt(videoGames.size()));
+
+    }
+
+    public VideoGame addVideoGame(VideoGame videoGame){
+        videoGame.generateId();
+        videoGames.add(videoGame);
+        return videoGame;
+    }
+
+    public VideoGame updateVideoGame(VideoGame updatedVideoGame){
+        VideoGame savedVideoGame = getVideoGameById(updatedVideoGame.getId());
+        savedVideoGame.setName(updatedVideoGame.getName());
+        savedVideoGame.setConsoleType(updatedVideoGame.getConsoleType());
+        //savedVideoGame.setMultiplayer(updatedVideoGame.getMultiplayer));
+        return savedVideoGame;
+    }
+
+    public void deleteVideoGame(int id){
+        for (int i = 0; i < videoGames.size(); i++) {
+            if(videoGames.get(i).getId()==id){
+                videoGames.remove(i);
+                break;
+            }
+        }
+
+    }
 }
+
+/*
+    private int id;
+    private String consoleType;
+    private String name;
+    private boolean multiplayer;
+ */
