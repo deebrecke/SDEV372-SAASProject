755a98fe08daddc75b153efa7d46cc37c07ac98d
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-10-18 12:06:44 -0700

    Updated Service classes to use repo

--- src/main/java/edu/greenriver/sdev/saasproject/services/BoardGameService.java
+++ src/main/java/edu/greenriver/sdev/saasproject/services/BoardGameService.java
@@ -1,41 +1,40 @@
 package edu.greenriver.sdev.saasproject.services;
 
+import edu.greenriver.sdev.saasproject.db.IBoardGamesRepository;
 import edu.greenriver.sdev.saasproject.models.BoardGame;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
 @Service
 public class BoardGameService {
-    private List<BoardGame> boardGames = new ArrayList<>(List.of(
-            new BoardGame("family", "Parcheesi", 2, 4),
-            new BoardGame("logic", "Terraforming Mars", 1, 5),
-            new BoardGame("party", "Cards Against Humanity", 4, 20),
-            new BoardGame("other", "Hive", 2, 2)
-    ));
+    private IBoardGamesRepository bgrepo;
+
+    public BoardGameService(IBoardGamesRepository bgrepo){
+        this.bgrepo = bgrepo;
+    }
 
     public List<BoardGame> getAllBoardGames(){
-        return boardGames;
+        return bgrepo.findAll();
     }
 
     public BoardGame getBoardGameById(int id){
-        Optional<BoardGame> found = boardGames.stream()
-                .filter(boardGame -> boardGame.getId()==id)
-                .findFirst();
+        Optional<BoardGame> found = bgrepo.findById(id)
+                .filter(boardGame -> boardGame.getId()==id);
         return found.orElse(null);
     }
 
     public BoardGame random(){
         Random rand = new Random();
+        List<BoardGame> boardGames = getAllBoardGames();
         return boardGames.get(rand.nextInt(boardGames.size()));
 
     }
 
     public BoardGame addBoardGame(BoardGame boardGame){
-        boardGames.add(boardGame);
+        boardGame = bgrepo.save(boardGame);
         return boardGame;
     }
 
@@ -45,17 +44,12 @@ public class BoardGameService {
         savedBoardGame.setCategory(updatedBoardGame.getCategory());
         savedBoardGame.setMinPlayers(updatedBoardGame.getMinPlayers());
         savedBoardGame.setMaxPlayers(updatedBoardGame.getMaxPlayers());
+        savedBoardGame = bgrepo.save(savedBoardGame);
         return savedBoardGame;
     }
 
     public void deleteBoardGame(int id){
-        for (int i = 0; i < boardGames.size(); i++) {
-            if(boardGames.get(i).getId()==id){
-                boardGames.remove(i);
-                break;
-            }
-        }
-
+        bgrepo.deleteById(id);
     }
 
 }//end of BoardGameService class
--- src/main/java/edu/greenriver/sdev/saasproject/services/VideoGameService.java
+++ src/main/java/edu/greenriver/sdev/saasproject/services/VideoGameService.java
@@ -1,41 +1,40 @@
 package edu.greenriver.sdev.saasproject.services;
 
+import edu.greenriver.sdev.saasproject.db.IVideoGamesRepository;
 import edu.greenriver.sdev.saasproject.models.VideoGame;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
 @Service
 public class VideoGameService {
-    private List<VideoGame> videoGames = new ArrayList<>(List.of(
-        new VideoGame("Wii", "Wii Fit", false),
-        new VideoGame("Oculus", "In Death: Unchained", false),
-        new VideoGame("Switch", "Mario Party", true)
-    ));
+    private IVideoGamesRepository vgrepo;
 
+    public VideoGameService(IVideoGamesRepository vgrepo){
+        this.vgrepo = vgrepo;
+    }
 
     public List<VideoGame> getAllVideoGames(){
-        return videoGames;
+        return vgrepo.findAll();
     }
 
     public VideoGame getVideoGameById(int id){
-        Optional<VideoGame> found = videoGames.stream()
-                .filter(videoGame -> videoGame.getId()==id)
-                .findFirst();
+        Optional<VideoGame> found = vgrepo.findById(id)
+                .filter(videoGame -> videoGame.getId()==id);
         return found.orElse(null);
     }
 
     public VideoGame random(){
         Random rand = new Random();
+        List<VideoGame> videoGames = getAllVideoGames();
         return videoGames.get(rand.nextInt(videoGames.size()));
 
     }
 
     public VideoGame addVideoGame(VideoGame videoGame){
-        videoGames.add(videoGame);
+        videoGame = vgrepo.save(videoGame);
         return videoGame;
     }
 
@@ -44,16 +43,11 @@ public class VideoGameService {
         savedVideoGame.setName(updatedVideoGame.getName());
         savedVideoGame.setConsoleType(updatedVideoGame.getConsoleType());
         savedVideoGame.setMultiplayer(updatedVideoGame.isMultiplayer());
+        savedVideoGame = vgrepo.save(savedVideoGame);
         return savedVideoGame;
     }
 
     public void deleteVideoGame(int id){
-        for (int i = 0; i < videoGames.size(); i++) {
-            if(videoGames.get(i).getId()==id){
-                videoGames.remove(i);
-                break;
-            }
-        }
-
+        vgrepo.deleteById(id);
     }
 }
