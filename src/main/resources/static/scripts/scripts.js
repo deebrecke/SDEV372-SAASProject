bc6958586ed0ac12b06282427a404d503aadf72e
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-11-08 13:12:39 -0800

    Edit function works with one small bug

--- src/main/resources/static/scripts/scripts.js
+++ src/main/resources/static/scripts/scripts.js
@@ -1,10 +1,14 @@
 window.onload = async function () {
     await fetchBoardGames();
     await fetchVideoGames();
+
+    console.log("onload() ended");
+
     let addBoardGameButton = document.querySelector("button#add-bg");
     addBoardGameButton.onclick = addNewBoardGame;
     let addVideoGameButton = document.querySelector("button#add-vg");
     addVideoGameButton.onclick = addNewVideoGame;
+
     let editVideoGameButton = document.querySelector("button#edit-vg");
     editVideoGameButton.onclick = editVideoGame;
 }
@@ -41,7 +45,6 @@ function addSingleBoardGameToTable(boardgame) {
     let tableCategory = document.createElement("td");
     let tableMin = document.createElement("td");
     let tableMax = document.createElement("td");
-    let editButton = document.createElement("td");
     let deleteButton = document.createElement("td")
 
     //connect them (parent to child)
@@ -51,7 +54,6 @@ function addSingleBoardGameToTable(boardgame) {
     row.appendChild(tableCategory)
     row.appendChild(tableMin)
     row.appendChild(tableMax)
-    row.appendChild(editButton)
     row.appendChild(deleteButton)
 
     //add text or HTML attributes
@@ -60,15 +62,7 @@ function addSingleBoardGameToTable(boardgame) {
     tableCategory.textContent = boardgame.category;
     tableMin.textContent = boardgame.minPlayers;
     tableMax.textContent = boardgame.maxPlayers;
-    editButton.textContent = "edit"
     deleteButton.textContent = "delete"
-}
-
-function addBoardGamesToTable(boardGameArray) {
-    for (let i = 0; i < boardGameArray.length; i++) {
-        let boardgame = boardGameArray[i];
-        addSingleBoardGameToTable(boardgame);
-    }
 }
 
 function addSingleVideoGameToTable(videogame) {
@@ -79,9 +73,8 @@ function addSingleVideoGameToTable(videogame) {
     let tableName = document.createElement("td");
     let tableConsole = document.createElement("td");
     let tableMulti = document.createElement("td");
-    //let editButton = document.createElement("td");
-    let deleteButton = document.createElement("td")
-    let deleteLink =document.createElement("a")
+    let deleteLinkCell = document.createElement("td")
+    let deleteLink = document.createElement("a");
 
     //connect them (parent to child)
     body.appendChild(row);
@@ -89,18 +82,22 @@ function addSingleVideoGameToTable(videogame) {
     row.appendChild(tableName)
     row.appendChild(tableConsole)
     row.appendChild(tableMulti)
-    //row.appendChild(editButton)
-    row.appendChild(deleteButton)
-    deleteButton.appendChild(deleteLink)
+    deleteLinkCell.appendChild(deleteLink)
+    row.appendChild(deleteLinkCell)
 
     //add text or HTML attributes
     tableId.textContent = videogame.id;
     tableName.textContent = videogame.name;
     tableConsole.textContent = videogame.consoleType;
     tableMulti.textContent = videogame.multiplayer;
-    //editButton.textContent = "edit"
-    deleteButton.textContent = "delete"
-    deleteLink.href = "home.html"
+    deleteLink.textContent = "delete"
+}
+
+function addBoardGamesToTable(boardGameArray) {
+    for (let i = 0; i < boardGameArray.length; i++) {
+        let boardgame = boardGameArray[i];
+        addSingleBoardGameToTable(boardgame);
+    }
 }
 
 function addVideoGamesToTable(videoGameArray) {
@@ -110,6 +107,37 @@ function addVideoGamesToTable(videoGameArray) {
     }
 }
 
+async function addNewBoardGame(event) {
+    //stop the form from submitting, we are using fetch() instead!
+    event.preventDefault();
+
+    let minInput = document.querySelector("input#bg-min").value;
+    let maxInput = document.querySelector("input#bg-max").value;
+
+    if (minInput > 0 && minInput <= 20 && maxInput > 0 && maxInput <= 20) {
+        let newBoardGame = {
+            name: document.querySelector("input#bg-name").value,
+            category: document.querySelector("input#bg-category").value,
+            minPlayers: minInput,
+            maxPlayers: maxInput
+        };
+
+        let uri = "http://localhost:8080/boardgames";
+        let config = {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newBoardGame)
+        };
+
+        let response = await fetch(uri, config);
+        let jsonObjectReturned = await response.json();
+        addSingleBoardGameToTable(jsonObjectReturned);
+    } else {
+        alert("Please enter a number between 1 - 20")
+    }
+}
 
 async function addNewVideoGame(event) {
     event.preventDefault();
@@ -143,95 +171,40 @@ async function addNewVideoGame(event) {
     }
 }
 
-async function addNewBoardGame(event) {
-    //stop the form from submitting, we are using fetch() instead!
+async function editVideoGame(event) {
     event.preventDefault();
 
-    let minInput = document.querySelector("input#bg-min").value;
-    let maxInput = document.querySelector("input#bg-max").value;
+    let editedVideoGame = {
+        id: document.querySelector("input#edit-vg-id").value,
+        consoleType: document.querySelector("input#edit-vg-console").value,
+        name: document.querySelector("input#edit-vg-name").value,
+        multiplayer: document.querySelector("input#edit-vg-multi").value
+    };
 
-    if (minInput > 0 && minInput <= 20 && maxInput > 0 && maxInput <= 20) {
-        let newBoardGame = {
-            name: document.querySelector("input#bg-name").value,
-            category: document.querySelector("input#bg-category").value,
-            minPlayers: minInput,
-            maxPlayers: maxInput
-        };
+    let uri = "http://localhost:8080/videogames";
+    let config = {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(editedVideoGame)
+    };
 
-        let uri = "http://localhost:8080/boardgames";
-        let config = {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newBoardGame)
-        };
+    let response = await fetch(uri, config);
+    let jsonObjectReturned = await response.json();
 
-        let response = await fetch(uri, config);
-        let jsonObjectReturned = await response.json();
-        addSingleBoardGameToTable(jsonObjectReturned);
-    } else {
-        alert("Please enter a number between 1 - 20")
+    let rows = document.querySelectorAll("#vg-tbody tr");
+
+    for (let i = 0; i < rows.length; i++) {
+        let tr = rows[i]
+
+        let tdId = tr.children[0];
+        let otherId = parseInt(tdId.textContent)
+
+        if (gameId === otherId) {
+            tr = editedVideoGame
+        }
     }
-}
-
-async function editVideoGame(event){
-    event.preventDefault();
-
-        let editedVideoGame = {
-            id: document.querySelector("input#edit-vg-id"),
-            name: document.querySelector("input#vg-name").value,
-            consoleType: document.querySelector("input#vg-console").value,
-            multiplayer: document.querySelector("input#vg-multi").value
-        };
-
-        let uri = "http://localhost:8080/videogames";
-        let config = {
-            method: "put",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(editedVideoGame)
-        };
-
-        let response = await fetch(uri, config);
-        let jsonObjectReturned = await response.json();
-
-        //addSingleVideoGameToTable(jsonObjectReturned);//NO, this adds a new blank line to the table
-        //figure out how to change the value here
-        //something needs to be done with jsonObjectReturned, but I can't figure out what
-
-}
 
 
-async function addNewBoardGame(event) {
-    //stop the form from submitting, we are using fetch() instead!
-    event.preventDefault();
-
-    let minInput = document.querySelector("input#bg-min").value;
-    let maxInput = document.querySelector("input#bg-max").value;
-
-    if (minInput > 0 && minInput <= 20 && maxInput > 0 && maxInput <= 20) {
-        let newBoardGame = {
-            name: document.querySelector("input#bg-name").value,
-            category: document.querySelector("input#bg-category").value,
-            minPlayers: minInput,
-            maxPlayers: maxInput
-        };
-
-        let uri = "http://localhost:8080/boardgames";
-        let config = {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newBoardGame)
-        };
-
-        let response = await fetch(uri, config);
-        let jsonObjectReturned = await response.json();
-        addSingleBoardGameToTable(jsonObjectReturned);
-    } else {
-        alert("Please enter a number between 1 - 20")
-    }
 }
